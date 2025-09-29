d6bc41e9e0f6cd13be52e046669f94d5f83300fd
Nora Elisa <nora.heikkila@helsinki.fi>
2025-09-29 11:13:48 +0300

    change in the file

--- src/training_data_preprocess.py
+++ src/training_data_preprocess.py
@@ -14,7 +14,7 @@ data = pd.read_csv('/Users/noraheikkila/ylesent/data/raw/fi.csv')
 data['text'] = data['text'].str.lower()                                          # lowercase the data
 data.drop_duplicates(inplace=True)                                               # drop the duplicatess and modify the existing df
 data = data[data['text'].notna()]                                                # clear the missing values
-data['text'] = data['text'].apply(lambda x: re.sub(r'[^a-zA-ZäöåÄÖÅ\s]', '', x)) # remoce from special characters, except letters and spaces
+data['text'] = data['text'].apply(lambda x: re.sub(r'[^a-zA-ZäöåÄÖÅ\s]', '', x)) # remove from special characters, except letters and spaces
 data['text'] = data['text'].apply(lambda x: re.sub(r'\s+', ' ', x).strip())      # normalize the spacing, so there is not double spacing
 print(data.head())
 
